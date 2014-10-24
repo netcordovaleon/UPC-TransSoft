677af0fa1ab5f65dcdb5b6890d15e760cc0f38c7
netcordovaleon <netcordovaleon@gmail.com>
2014-10-25 10:57:52 +1200

    Proyecto Web MVC Inicial

---
+++ transSoft/UPC.TS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UPC.TS.Web.Startup))]
+namespace UPC.TS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
